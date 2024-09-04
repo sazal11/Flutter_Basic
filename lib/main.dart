aca3c70542c6c45b1e64e1c124abaf5c2c8b7799
sajal shrestha <sajal.stha123@hotmail.co.uk>
2024-09-04 23:40:10 +0545

    ListView.builder example one

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ListViewExample1());
+  runApp(const ListViewBuilderExample1());
 }
 
 class MyApp extends StatelessWidget {
@@ -817,35 +817,76 @@ class ListViewExample1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar( backgroundColor: Colors.orange,title: const Text('List View Example')),
-          
+          appBar: AppBar(
+              backgroundColor: Colors.orange,
+              title: const Text('List View Example')),
           body: ListView(
             scrollDirection: Axis.vertical,
             reverse: true,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('One',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                child: Text(
+                  'One',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('Two',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                child: Text(
+                  'Two',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('Three',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                child: Text(
+                  'Three',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('Four',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                child: Text(
+                  'Four',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('Five',style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+                child: Text(
+                  'Five',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+                ),
               ),
-              
-
             ],
+          )),
+    );
+  }
+}
+
+class ListViewBuilderExample1 extends StatelessWidget {
+  const ListViewBuilderExample1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var arrNames = ['Ram', 'Shyam', 'Hari', 'Kumar', 'Shova', 'Rahul', 'Sita'];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.orange,
+              title: const Text('List View Example')),
+          body: ListView.builder(
+            itemBuilder: (context, index) {
+              return Text(
+                arrNames[index],
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              );
+            },
+            itemCount: arrNames.length,
+            //  reverse: true,
+            itemExtent: 100,
+            //scrollDirection: Axis.horizontal,
           )),
     );
   }
